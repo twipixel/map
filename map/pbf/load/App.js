8b73a86c9ab1642f36ed1d4306cfae438ec8a994
Choi Sungryeol <choi.sungryeol@navercorp.com>
2019-11-08 18:07:28 +0900

    Implement pbf getFeatures

--- map/pbf/load/App.js
+++ map/pbf/load/App.js
@@ -1,104 +1,74 @@
+import axios from 'axios';
+import geobuf from 'geobuf';
+import Pbf from 'pbf';
+import ProtoBuf from 'pbf';
+import { VectorTile } from '@mapbox/vector-tile';
+
+const xyz = {x: 27953, y: 12711, z: 15};
+const bus = 'https://map.pstatic.net/nvb/wmts/bus/d2e364c9-b75e-4fd7-8020-c4b26f82b9ce/getTile/27953/12711/15/pbf';
+const poi_web = 'https://map.pstatic.net/nvb/wmts/poi_web/8fe6883e-5525-45f8-8f55-5e30bc4981f1/getTile/27953/12711/15/pbf';
+
 export default class App {
-    constructor() {
-        this.initialize();
-        this.loadPbf();
-        this.loadVector();
+  constructor() {
+    this.loadVector();
+  }
+
+  loadVector() {
+    this.load({
+      url: poi_web,
+      responseType: 'arraybuffer',
+    }).then(buffer => {
+      // const pbf = new Pbf(new Uint8Array(response));
+      // const tile = new VectorTile(pbf);
+      const tile = new VectorTile(new ProtoBuf(buffer));
+      console.log('tile', tile);
+
+      const collection = this.getFeatures(tile);
+      console.log('features', collection.features);
+    });
+  }
+
+  load({url, responseType = 'json'}) {
+    const fetchData = () => {
+      return axios({
+        url,
+        responseType,
+      }).then(({data}) => {
+        return data;
+      });
+    };
+    return fetchData();
+  }
+
+  getFeatures(tile) {
+    let collection = {
+      type: 'FeatureCollection',
+      features: []
+    };
+
+    let layers = Object.keys(tile.layers);
+
+    if (!Array.isArray(layers)) {
+      layers = [layers];
     }
 
-    initialize() {
-        const canvas = document.getElementById('map');
-        const ctx = canvas.getContext('2d');
-        console.log('canvas', canvas);
-        console.log('ctx', ctx);
-    }
+    layers.forEach((layerID) => {
+      const layer = tile.layers[layerID];
 
-    loadPbf() {
-        this.load(
-            './../../../asset/countries/0/0/0.pbf',
-            (xhttp) => {
-                const pbf = new Pbf(new Uint8Array(xhttp.response));
-                // const tile = new VectorTile(pbf);
-                console.log('pbf', pbf);
-                // console.log('tile', tile);
-            },
-            (xhttp) => {
-                console.log(xhttp.statusText);
-            }
-        );
-    }
+      const len = layer.length;
+      for (let i = 0; i < len; i++) {
 
-    loadVector() {
-        this.load(
-            'https://tile.mapzen.com/mapzen/vector/v1/all/13/1308/3165.mvt?api_key=vector-tiles-LM25tq4',
-            // 'https://tile.mapzen.com/mapzen/vector/v1/all/13/1309/3165.mvt?api_key=vector-tiles-LM25tq4',
-            // 'https://tile.mapzen.com/mapzen/vector/v1/all/13/1310/3165.mvt?api_key=vector-tiles-LM25tq4',
-            // 'https://tile.mapzen.com/mapzen/vector/v1/all/13/1311/3165.mvt?api_key=vector-tiles-LM25tq4',
-            // 'https://tile.mapzen.com/mapzen/vector/v1/all/13/1308/3166.mvt?api_key=vector-tiles-LM25tq4',
-            // 'https://tile.mapzen.com/mapzen/vector/v1/all/13/1309/3166.mvt?api_key=vector-tiles-LM25tq4',
-            // 'https://tile.mapzen.com/mapzen/vector/v1/all/13/1310/3166.mvt?api_key=vector-tiles-LM25tq4',
-            // 'https://tile.mapzen.com/mapzen/vector/v1/all/13/1311/3166.mvt?api_key=vector-tiles-LM25tq4',
-            // 'https://tile.mapzen.com/mapzen/vector/v1/all/13/1308/3167.mvt?api_key=vector-tiles-LM25tq4',
-            // 'https://tile.mapzen.com/mapzen/vector/v1/all/13/1309/3167.mvt?api_key=vector-tiles-LM25tq4',
-            (xhttp) => {
-                const pbf = new Pbf(new Uint8Array(xhttp.response));
-                const tile = new VectorTile(pbf);
-                console.log('pbf', pbf);
-                console.log('tile', tile);
-
-                const zoom = 10;
-                const layers = ['water', 'landuse', 'roads', 'buildings'];
-
-                const data = {};
-                for (let key in tile.layers) {
-                    data[key] = tile.layers[key].toGeoJSON();
-                }
-
-                const features = [];
-
-                layers.forEach(
-                    (layer) => {
-                        if (data[layer]) {
-                            for (let i in data[layer].features) {
-                                // Don't include any label placement points
-                                if (data[layer].features[i].properties.label_placement) { continue }
-
-                                // Don't show large buildings at z13 or below.
-                                if (zoom <= 13 && layer == 'buildings') { continue }
-
-                                // Don't show small buildings at z14 or below.
-                                if (zoom <= 14 && layer == 'buildings' && data[layer].features[i].properties.area < 2000) { continue }
-
-                                data[layer].features[i].layer_name = layer;
-                                features.push(data[layer].features[i]);
-                            }
-                        }
-                    }
-                );
-                console.log('data', data);
-                console.log('features', features);
-            }
-        );
-    }
-
-    load(url, success = null, error = null) {
-        const xhttp = new XMLHttpRequest();
-        xhttp.open('GET', url);
-        xhttp.responseType = 'arraybuffer';
-        xhttp.onreadystatechange = () => {
-            if (xhttp.readyState == 4 && xhttp.status == 200) {
-                if (success) {
-                    success.call(null, xhttp);
-                }
-            }
+        const feature = {
+          ...layer.feature(i).toGeoJSON(xyz.x, xyz.y, xyz.z)
         };
-        xhttp.onerror = () => {
-            if (error) {
-                error.call(null, xhttp);
-            }
-        };
-        xhttp.send();
-    }
 
-    resize() {
-    }
+        collection.features.push(feature);
+      }
+    });
+
+    return collection;
+  }
+
+  resize() {
+  }
 }
