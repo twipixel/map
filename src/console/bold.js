a0c9f7a38699d6cf0ba5ce503e1a184793d3885e
Choi Sungryeol <choi.sungryeol@navercorp.com>
2019-11-15 19:20:55 +0900

    Update console

--- src/console/bold.js
+++ src/console/bold.js
@@ -1,5 +1,10 @@
-var boldPrefix = '▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨ ';
+const LABEL_END = 30;
+const PREFIX = '▨▨▨ ';
+const EMPTY_STRING = ' ';
 
 export default function bold(label) {
-  return `${boldPrefix} ${label}`;
+  const decorateLabel = `${PREFIX} ${label}`;
+  const labelLength = decorateLabel.length;
+  const emptyString = labelLength < LABEL_END ? Array(LABEL_END - labelLength).join(EMPTY_STRING) + EMPTY_STRING : '';
+  return decorateLabel + emptyString;
 }
